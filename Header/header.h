c109382ec63c8cbb8898c9e42a70d1ff6b8cce38
Naishadh <naishadhnimbark@gmail.com>
2014-01-09 12:35:39 +0530

    uart header added

---
+++ Header/header.h
@@ -0,0 +1,18 @@
+/*
+ * header.h
+ *
+ *  Created on: Dec 31, 2013
+ *      Author: acer
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+
+extern void send_uart(char *);
+extern void gnrt_fre(long int);
+extern void dir_config(void);
+extern void clock_calib(void);
+extern float ADC_config(void);
+
+#endif /* HEADER_H_ */
