61e430c57ed4832d369f07b83a7af625b21d0567
JB ELECTRONICS <JB ELECTRONICS@JBELECTRONICS>
2014-01-29 15:13:15 +0530

    Global Initialization.

---
+++ Source Files/msp430_global.c
@@ -0,0 +1,34 @@
+/********************************************************************//**
+* $Id$		msp430_global.c
+*//**
+* @file		msp430_global.c
+* @brief	Contains Global Variables of MSP430f6736
+* @version	1.0
+* @date		28. Jan. 2014
+* @author	Pankil.Edutech
+***********************************************************************/
+
+/** @addtogroup GLOBAL_Variables
+ * @{
+ */
+
+/**
+ *   Systick
+ */
+
+unsigned int hrt_bt_timer = 100;			// Initialized for 1 ms.
+unsigned int delay_timer = 0;
+
+/**
+ * @}
+ */
+
+
+
+/* End of Public Functions ---------------------------------------------------- */
+
+/**
+ * @}
+ */
+
+/* --------------------------------- End Of File ------------------------------ */
